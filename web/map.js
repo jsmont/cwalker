7aaa2cbfbab83767ea075e56ade3ad715f1f4f12
Jordi Solà <jsmont.sol@gmail.com>
2016-07-22 08:20:45 +0200

    added map page

---
+++ web/map.js
@@ -0,0 +1,14 @@
+function initMap() {
+    var mapDiv = document.getElementById('map');
+    map = new google.maps.Map(mapDiv, {
+        center: {lat: 41.3947687, lng: 2.0785561},
+        zoom: 12
+    });
+
+    google.maps.event.addListener(map, 'click', function(event) {
+       $("#buttonDiv").slideDown();
+       placeMarker(event.latLng);
+       form.location = event.latLng;
+    });
+    placesService = new google.maps.places.PlacesService(map);
+};

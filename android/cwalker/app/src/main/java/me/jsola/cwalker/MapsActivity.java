dfb9bf76e7beebaf4aa7ae3b0034b7764d58c284
jsola <jsmont.sol@gmail.com>
2016-07-22 10:08:59 +0200

    Done.

--- android/cwalker/app/src/main/java/me/jsola/cwalker/MapsActivity.java
+++ android/cwalker/app/src/main/java/me/jsola/cwalker/MapsActivity.java
@@ -1,14 +1,23 @@
 package me.jsola.cwalker;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
@@ -23,7 +32,10 @@ import java.util.List;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private LocationManager myLocationManager;
+    private LocationListener myLocationListener;
+    private Location myLocation;
+    private Marker myLocationMarker;
     private String result;
 
     @Override
@@ -56,8 +68,92 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         drawPath(result);
 
+        startLocationTracking();
+
     }
 
+    public void startLocationTracking() {
+
+        myLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        setUpListener();
+        setUpInitialLocation();
+
+    }
+
+    public void setUpListener(){
+        // Define a listener that responds to location updates
+        myLocationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                setNewLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        myLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
+        Log.d("Location Tracking","Listener started");
+    }
+
+    public void setUpInitialLocation(){
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+
+
+        Location InitialLocation = myLocationManager.getLastKnownLocation(locationProvider);
+
+        myLocation = InitialLocation;
+        setUpInitialMarker(InitialLocation);
+    }
+
+    public void setUpInitialMarker(Location InitiaLocation){
+
+        LatLng myLaLn = new LatLng(InitiaLocation.getLatitude(),
+                InitiaLocation.getLongitude());
+
+        MarkerOptions markerOpts = new MarkerOptions().position(myLaLn).title(
+                "my Location");
+        myLocationMarker = mMap.addMarker(markerOpts);
+
+        CameraPosition camPos = new CameraPosition.Builder().target(myLaLn)
+                .zoom(14).bearing(0).tilt(0).build();
+
+        CameraUpdate camUpd3 = CameraUpdateFactory.newCameraPosition(camPos);
+        mMap.animateCamera(camUpd3);
+
+
+    }
+
+    public void setNewLocation(Location CurrentLocation) {
+        LatLng newLatLng = new LatLng(CurrentLocation.getLatitude(),
+                CurrentLocation.getLongitude());
+
+        updateMarker(newLatLng);
+        updateCamera(newLatLng);
+
+        Log.d("Location Tracking", "New Location\tLatitude: "+CurrentLocation.getLatitude() + "\tLongitude: "+CurrentLocation.getLongitude());
+
+    }
+
+    public void updateMarker(LatLng newLatLng){
+        myLocationMarker.setPosition(newLatLng);
+    }
+
+
+    public void updateCamera(LatLng newLatLng){
+        CameraPosition camPos = new CameraPosition.Builder().target(newLatLng)
+                .zoom(mMap.getCameraPosition().zoom).bearing(0).tilt(0).build();
+
+        CameraUpdate camUpd3 = CameraUpdateFactory.newCameraPosition(camPos);
+        mMap.animateCamera(camUpd3);
+
+    }
     public void drawPath(String  result) {
 
         try {
